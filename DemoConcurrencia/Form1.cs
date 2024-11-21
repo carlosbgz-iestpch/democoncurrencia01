6a653cd2c1fa15071b41de9403a67ab790e7d751
Carlos Guerrero <carlosbgz@gmail.com>
2024-11-21 12:54:19 -0500

    Agregar archivos de proyecto.

---
+++ DemoConcurrencia/Form1.cs
@@ -0,0 +1,95 @@
+namespace DemoConcurrencia
+{
+    public partial class frmConcurrencia : Form
+    {
+        private CancellationTokenSource _cts;
+        public frmConcurrencia()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSecuencial_Click(object sender, EventArgs e)
+        {
+            ActualizarResultado("Iniciando proceso secuencial...");
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(2000);
+                ActualizarResultado($" Actividad - paso {i + 1}");
+            }
+            ActualizarResultado("Fin del proceso secuencial.");
+        }
+
+        private void ActualizarResultado(string mensaje)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => ActualizarResultado(mensaje)));
+                return;
+            }
+            txtResultado.AppendText($"{DateTime.Now:HH:mm:ss.fff}:{mensaje}{Environment.NewLine}");
+        }
+
+        private void btnIniciarHilo_Click(object sender, EventArgs e)
+        {
+            _cts = new CancellationTokenSource();
+            var token = _cts.Token;
+
+            Thread hilo1 = new Thread(() =>
+            {
+                try
+                {
+                    ActualizarResultado($"Iniciar nuevo thread {Thread.CurrentThread.ManagedThreadId}...");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        token.ThrowIfCancellationRequested ();
+
+                        Thread.Sleep(2000);
+                        ActualizarResultado($" Actividad en hilo {Thread.CurrentThread.ManagedThreadId} - Paso {i + 1}");
+                    }
+
+                    ActualizarResultado($"Thread {Thread.CurrentThread.ManagedThreadId} terminado.");
+                }
+                catch (OperationCanceledException)
+                {
+
+                    ActualizarResultado("Hilo cancelado.");
+                }
+            });
+            hilo1.Start();
+        }
+
+        private async void btnIniciarTarea_Click(object sender, EventArgs e)
+        {
+            _cts = new CancellationTokenSource();
+            var token = _cts.Token;
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    ActualizarResultado($"Iniciando Task {Task.CurrentId}...");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        token.ThrowIfCancellationRequested ();
+
+                        Thread.Sleep(2000);
+                        ActualizarResultado($" Actividad en tarea {Task.CurrentId} - Paso {i + 1}");
+                    }
+
+                    ActualizarResultado($"Task {Task.CurrentId} completada");
+                }
+                catch (OperationCanceledException)
+                {
+
+                    ActualizarResultado("Tarea cancelada.");
+                }
+                
+            });
+        }
+
+        private void btnCancelarHilo_Click(object sender, EventArgs e)
+        {
+            _cts?.Cancel();
+        }
+    }
+}
